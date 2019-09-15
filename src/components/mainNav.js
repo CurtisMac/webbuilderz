f5fb054394059aa9440d103da4c539a8a76265e6
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-09-15 20:28:51 +0100

    adds media queries to mainNav

--- src/components/mainNav.js
+++ src/components/mainNav.js
@@ -1,25 +1,77 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
-//Code below is for example only, it is not configured for a nav bar
+//Components
+
+//Assets
+import logo from "../data/img/logo.svg"
+
+//Styles
+const Container = styled.header`
+  display: flex;
+  flex-flow: row nowrap;
+  height: ${props => props.theme.size.navHeader};
+  margin: 0 25px 0;
+  @media screen and (max-width: ${props => props.theme.screen.medium}) {
+    flex-flow: column;
+    align-items: center;
+  }
+`
+const Logo = styled.img`
+  width: 30vw;
+  padding-top: 20px;
+  @media screen and (max-width: ${props => props.theme.screen.large}) {
+    width: 45vw;
+  }
+  @media screen and (max-width: ${props => props.theme.screen.medium}) {
+    width: 60vw;
+  }
+  @media screen and (max-width: ${props => props.theme.screen.small}) {
+    width: 90vw;
+  }
+`
+const LinkList = styled.ul`
+  list-style-type: none;
+  margin: 0 0 0 25px;
+  padding: 0;
+  padding-top: 25px;
+  display: flex;
+  flex-flow: row nowrap;
+  @media screen and (max-width: ${props => props.theme.screen.medium}) {
+    padding-top: 0;
+  }
+`
+const ListItem = styled.li`
+  margin: 10px 10px 0 0;
+  padding: 0;
+  text-transform: uppercase;
+`
+const StyledLink = styled(Link)`
+  color: ${props => props.theme.colors.sec1};
+  text-decoration: none;
+  :hover {
+    color: ${props => props.theme.colors.pri3};
+  }
+`
+
+//JS
 const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
+  <ListItem>
+    <StyledLink to={props.to}>{props.children}</StyledLink>
+  </ListItem>
 )
 
-export default ({ children }) => (
-  <div>
-    <header>
-      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline` }}>MySweetSite</h3>
-      </Link>
-      <ul style={{ listStyle: `none`, float: `right` }}>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/contact/">Contact</ListLink>
-      </ul>
-    </header>
-    {children}
-  </div>
+//Main Component
+export default () => (
+  <Container>
+    <Link to="/">
+      <Logo src={logo} alt="webbuilderz logo" />
+    </Link>
+    <LinkList>
+      <ListLink to="/">Home</ListLink>
+      <ListLink to="/about/">About</ListLink>
+      <ListLink to="/contact/">Contact</ListLink>
+    </LinkList>
+  </Container>
 )
