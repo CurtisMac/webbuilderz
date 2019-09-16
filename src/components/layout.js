bab9a39952eb4bffd14e1de43867ce07e0698a55
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-09-16 08:43:51 +0100

    adds mainNav into layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,17 +4,22 @@ import { theme } from "./theme"
 import { Helmet } from "react-helmet"
 import "../utils/styles.css"
 
+import NavHeader from "../components/mainNav"
+
 export default ({ children }) => (
   <div className="application">
     <Helmet>
       <title>WebBuilderz</title>
       <link
-        href="https://fonts.googleapis.com/css?family=Montserrat&display=swap"
+        href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap"
         rel="stylesheet"
       />
     </Helmet>
     <ThemeProvider theme={theme}>
-      <>{children}</>
+      <>
+        <NavHeader />
+        {children}
+      </>
     </ThemeProvider>
   </div>
 )
