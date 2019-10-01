acd89345a31bfc3335b9ae0a6c6845ff90634f32
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-10-01 13:02:00 +0100

    adds styledButton.js

---
+++ src/components/styledButton.js
@@ -0,0 +1,24 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+//Styles
+const Button = styled(Link)`
+  background-color: ${props => props.theme.colors.pri1};
+  color: ${props => (props.primary ? "black" : "blue")};
+  padding: 15px 30px;
+  text-decoration: none;
+  border: none;
+  border-radius: 25px;
+  font-weight: bold;
+  text-align: center;
+`
+
+//Component
+const index = props => (
+  <Button {...props} to={props.to}>
+    {props.children}
+  </Button>
+)
+
+export default index
