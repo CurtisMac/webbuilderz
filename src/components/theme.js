0dc1d928fc13707fd84dff77052e0d34c704074c
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-10-09 11:54:13 +0100

    adds screen sizes to themes component

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,11 +1,30 @@
 export const theme = {
   colors: {
-    pri1: "#eaaa4b",
-    pri2: "#ffbe4f",
-    sec1: "#3d323d",
+    pri1: "#eaaa4b", //main yellow
+    pri2: "#ffbe4f", //Complimentary yellow
+    pri3: "#5e9b9b", //teal
+    pri4: "#b8336a", //magenta
+    sec1: "#3d323d", //Brown || was #3d233d should be #3d323d ??
     sec2: "#1e101e",
+    sec3: "#392f5a", //purple
+    error: "red",
+    shadow: "#929292",
   },
   gradients: {
     white: "linear-gradient( #fff, #f9f9ff)",
+    colour: "linear-gradient(to bottom left, #392f5a, #5e9b9b)", //background wave colour
+    dark: "linear-gradient( #392f5a, #3d323d)",
+  },
+  size: {
+    navHeader: "120px",
+  },
+  screen: {
+    xsmall: "320px",
+    small: "480px",
+    medium: "768px",
+    large: "1024px",
+  },
+  shadow: {
+    box: "3px 3px 10px #929292",
   },
 }
