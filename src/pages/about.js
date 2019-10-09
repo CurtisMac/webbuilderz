76f565a53b29d21f756bb89ebc99c61435b727d7
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-10-09 14:08:54 +0100

    removes depreciated reference to head component in about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,8 @@
 import React from "react"
-
-import Header from "../components/header"
 import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <Header headerText="About WebBuilderz" />
     <p>Such wow. Very React.</p>
   </Layout>
 )
