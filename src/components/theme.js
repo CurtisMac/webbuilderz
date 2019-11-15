707e03e49cb175256cf8fed7e7e2916c393f3657
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-11-15 11:39:46 +0000

    updates theme.js with new color scheme

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,14 +1,22 @@
 export const theme = {
   colors: {
+    //Primary Colors
     pri1: "#eaaa4b", //main yellow
-    pri2: "#ffbe4f", //Complimentary yellow
+    pri2: "#392F5A", //Complimentary yellow
     pri3: "#5e9b9b", //teal
     pri4: "#b8336a", //magenta
+    //Accent Colors
+    acc1: "#3D323D",
+    acc2: "#FFBE4F",
+    acc3: "#1E101E",
+    acc4: "#6194CE ",
+    //Utility Colors
+    error: "#C0392B",
+    shadow: "rgba(0,0,0,0.16)",
+    //old - ensure these are not being used and then delete
     sec1: "#3d323d", //Brown || was #3d233d should be #3d323d ??
     sec2: "#1e101e",
     sec3: "#392f5a", //purple
-    error: "red",
-    shadow: "#929292",
   },
   gradients: {
     white: "linear-gradient( #fff, #f9f9ff)",
@@ -25,6 +33,7 @@ export const theme = {
     large: "1024px",
   },
   shadow: {
-    box: "3px 3px 10px #929292",
+    box: `3px 3px 7px rgba(0,0,0,0.16)`,
+    boxActive: `0.7px 0.7px 3px rgba(0,0,0,0.16)`,
   },
 }
