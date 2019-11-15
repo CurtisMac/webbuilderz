ebf2614cdc45468b96999d0a5bbad369637cc34d
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-11-15 11:40:26 +0000

    adds inverted styed to styledButton.js

--- src/components/styledButton.js
+++ src/components/styledButton.js
@@ -3,9 +3,10 @@ import styled from "styled-components"
 import { Link } from "gatsby"
 
 //Styles
-const Button = styled(Link)`
-  background-color: ${props => props.theme.colors.pri1};
-  color: ${props => (props.primary ? "black" : "blue")};
+const PrimaryButton = styled(Link)`
+  background-color: ${props =>
+    props.theme.colors[props.color] || props.theme.colors.pri1};
+  color: white;
   padding: 15px 30px;
   text-decoration: none;
   border: none;
@@ -13,13 +14,37 @@ const Button = styled(Link)`
   font-weight: bold;
   text-align: center;
   box-shadow: ${props => props.theme.shadow.box};
+  min-width: 100px;
+  display: inline-block;
+  :hover {
+    transform: scale(1.03);
+  }
+  :active {
+    transform: scale(1);
+    box-shadow: ${props => props.theme.shadow.boxActive};
+  }
+`
+const InvertedButton = styled(PrimaryButton)`
+  color: ${props => props.theme.colors[props.color]};
+  border: 1.5px solid ${props => props.theme.colors[props.color]};
+  background-color: white;
 `
 
 //Component
-const index = props => (
-  <Button {...props} to={props.to}>
-    {props.children}
-  </Button>
-)
+const index = props => {
+  if (props.invert) {
+    return (
+      <InvertedButton {...props} to={props.to}>
+        {props.children}
+      </InvertedButton>
+    )
+  } else {
+    return (
+      <PrimaryButton {...props} to={props.to}>
+        {props.children}
+      </PrimaryButton>
+    )
+  }
+}
 
 export default index
